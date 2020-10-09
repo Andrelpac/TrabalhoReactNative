9f5914b1d63f0eba653676eedb05c1e71a413f05
Ronaldo Ramos <ronaldo.rosario.ramos@gmail.com>
2020-10-08 23:00:26 -0300

    splash Screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import StackNavigator from './navigators/StackNavigator';
 import DrawNavigator from './navigators/DrawerNavigator';
+import SplashScreen from 'react-native-splash-screen';
 
 const App = () => {
+  useEffect(() =>{
+    SplashScreen.hide();
+  }, []);
+  
   return (
     <NavigationContainer>
       <StackNavigator>
